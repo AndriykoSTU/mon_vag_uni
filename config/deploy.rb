4d58d99effcbcb8a9c567990ef9b3d8714201109
AndriykoSTU <balfagor@bigmi.net>
2015-12-16 23:19:52 +0200

    +capistrano unicorn | set file

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'myapp'
+
+
+
+set :scm, :git
+set :repo_url, 'https://AndriykoSTU:deusexmach1na@github.com/AndriykoSTU/mon_vag_uni'
+set :branch, 'master'
+
+
+#set :use_sudo, false
+#set :bundle_binstubs, nil
+set :linked_files, fetch(:linked_files, []).push('config/mongoid.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets','vendor/bundle', 'public/system')
+
+
+
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
+
+
+namespace :mongoid do
+  desc "Copy mongoid config"
+  task :copy do
+  	on roles(:all) do
+  	   execute "mkdir -p #{shared_path}/config"
+  	   #execute "touch #{shared_path}/config/mongoid.yml"
+end
+  	run_locally do |cop|
+    execute "scp config/mongoid.yml deployer@192.168.44.10:/#{shared_path}/config/"
+  end
+end
+end
+
+before "deploy:starting", "mongoid:copy"
---
+++ config/unicorn/production.rb
@@ -0,0 +1,49 @@
+root = "/home/deployer/apps/myapp/current"
+working_directory root
+
+pid "#{root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+worker_processes 3
+timeout 30
+preload_app true
+
+listen '/tmp/unicorn.myapp.sock', backlog: 64
+before_fork 
+old_pid = '#{root}/tmp/pids/unicorn.pid.oldbin'
+if File.exists?(old_pid) && server.pid != old_pid
+  begin
+    Process.kill("QUIT", File.read(old_pid).to_i)
+  rescue Errno::ENOENT, Errno::ESRCH
+    # someone else did our job for us
+  end
+end
+
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  #defined?(ActiveRecord::Base) and
+    #ActiveRecord::Base.connection.disconnect!
+end
+
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  #defined?(ActiveRecord::Base) and
+   # ActiveRecord::Base.establish_connection
+end
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+end
